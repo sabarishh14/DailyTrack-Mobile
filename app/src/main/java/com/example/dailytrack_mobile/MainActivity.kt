4a6a84e35d4b7356c2b5c19bf23729b6541bb46f
Marxalpha <saikrishnaknm@gmail.com>
2026-09-04 10:27:03 +0530

    feat: added logo

--- app/src/main/java/com/example/dailytrack_mobile/MainActivity.kt
+++ app/src/main/java/com/example/dailytrack_mobile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -54,6 +55,7 @@ class MainActivity : FragmentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
